dfc4e55fc5f5352a1337abc2fbb1a19348086645
anco-sam-franco-b <ancosamfranco250@gmail.com>
2026-05-09 10:54:29 +0200

    update-server

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,18 @@ app.get('/about', (req, res)=>{
     res.send('This is our about route')
 })
 
+app.post('/create', (req, res)=>{
+    res.status(201).json({
+        message: 'Item Created!'
+    })
+})
+app.put('/update/:item_id', (req, res)=>{
+    const {item_id}=req.params
+    res.status(200).json({
+        message: `items updated with this ${item_id}`
+    })
+})
+
 //starting server
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
